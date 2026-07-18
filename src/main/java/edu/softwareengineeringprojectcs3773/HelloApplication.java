9df21f4a8029aa120cf61dc510860e03e0b6557b
AbelSotelo0827 <abelsotelo07@gmail.com>
2026-07-17 20:58:03 -0500

    added onaction events for login screen buttons

--- src/main/java/edu/softwareengineeringprojectcs3773/HelloApplication.java
+++ src/main/java/edu/softwareengineeringprojectcs3773/HelloApplication.java
@@ -6,18 +6,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+
+import edu.softwareengineeringproject3773.controller.LoginController;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        FXMLLoader fxmlLoader = new FXMLLoader(getURL("login-screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        
+        LoginController loginController = fxmlLoader.getController();
+        
+        FXMLLoader registerLoader = new FXMLLoader(getURL("register-screen.fxml"));
+        Scene registerScene = new Scene(registerLoader.load());
+        
+        loginController.setStage(stage);
+        loginController.setRegisterScene(registerScene);
+        
+        
         stage.setTitle("RoadRunner MarketPlace!");
         stage.setScene(scene);
         stage.show();
     }
+    
+    public static URL getURL(String fileName) {
+    	return HelloApplication.class.getResource(fileName);
+    }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
